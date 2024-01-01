093edb7504f1b655fc8ac0b6a6f6e12aa0874a46
n-sweep <34486798+n-sweep@users.noreply.github.com>
2024-01-01 10:36:07 -0500

    sql experimenting

--- time_diff.sql
+++ time_diff.sql
@@ -1,21 +1,66 @@
 WITH tbl AS (
-    SELECT *,
-        SUM(CASE WHEN event_type = 'pomo' THEN 1 ELSE 0 END)
-        OVER(ORDER BY timestamp) AS pomo_id
+    SELECT
+        id,
+        SUM(CASE WHEN event_type in ('pomo', 'break', 'stop') THEN 1 ELSE 0 END)
+            OVER(ORDER BY timestamp)
+        AS pomo_id,
+        event_type,
+        len,
+        timestamp,
+        DATETIME(timestamp, '+' || len || ' minute') AS target
     FROM
         pomo
 ),
-time_diff AS (
+pause_ids AS (
     SELECT
-        CAST(diff / 60 AS INT) AS min,
-        CAST(diff % 60 AS INT) AS sec
+        event_type,
+        timestamp,
+        SUM(CASE WHEN event_type = 'pause' THEN 1 ELSE 0 END)
+            OVER(PARTITION BY pomo_id ORDER BY timestamp)
+        + (10 * pomo_id) AS pause_id
+    FROM
+        tbl
+    WHERE event_type in ('pause', 'unpause')
+),
+pauses AS (
+    SELECT *
     FROM (
         SELECT *,
-            (JULIANDAY(DATETIME(timestamp, '+' || len || ' minute'))
-             - JULIANDAY(CURRENT_TIMESTAMP)) * 24 * 60 * 60 AS diff
+            timestamp AS pause,
+            LEAD(timestamp) OVER (PARTITION BY pause_id ORDER BY timestamp) AS unpause
+        FROM
+            pause_ids
+    )
+),
+time_remain AS (
+    SELECT id,
+        sec AS total_seconds,
+        CAST(sec / 60 AS INT)
+        || ":" ||
+        ABS(CAST(sec % 60 AS INT)) AS remain_string
+    FROM (
+        SELECT
+            id,
+            (JULIANDAY(target) - JULIANDAY(
+                CASE WHEN pomo_id = (SELECT MAX(pomo_id) FROM tbl)
+                    THEN CURRENT_TIMESTAMP
+                    ELSE LEAD(timestamp) OVER(ORDER BY timestamp)
+                END
+            )) * 24 * 60 * 60 AS sec
         FROM tbl
-        WHERE event_type = 'pomo'
+        WHERE event_type in ('pomo', 'break')
     )
 )
 
-SELECT * FROM tbl
+-- SELECT
+    -- p.id,
+    -- pomo_id,
+    -- event_type,
+    -- len,
+    -- timestamp,
+    -- total_seconds,
+    -- remain_string
+-- FROM tbl p
+-- LEFT JOIN time_remain t
+-- ON p.id = t.id
+select * from pauses
